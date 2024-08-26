a16960bb3bde6e76e6575a97f76e2ec83b5e6990
fullstackdev44 <fullstackdeveloper44@gmail.com>
2024-08-26 11:41:09 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const multer = require('multer');
+const path = require('path');
+const cors = require('cors');
+const app = express();
+const port = 3000;
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads/') 
+    },
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + '-' + file.originalname) 
+    }
+});
+const upload = multer({ storage: storage });
+app.use(express.static(path.join(__dirname, 'examples')));
+app.post('/upload', upload.single('pdf'), (req, res) => {
+   
+    res.send("File uploaded successfully");
+});
+
+// Serve the HTML form
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'examples', 'index.html'));
+});
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
